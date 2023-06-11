28f7fc3be4161f1440dbc477390ba2e95810f587
givikojikia5@gmail.com <givikojikia5@gmail.com>
2023-06-11 18:15:39 +0400

    with gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,174 @@
 import { useRef, useState } from "react";
 import { Divide as Hamburger } from "hamburger-react";
-import { motion } from "framer-motion"
+import { motion, transform } from "framer-motion";
 
 import styled from "styled-components";
-import videoBG from "../public/bg-video.mp4";
+import videoBG from "/bg-video.mp4";
+
+const imgArray = ["/1.jpg", "/2.jpg", "/3.jpg", "/4.jpg", "/5.jpg", "/6.jpg"];
 
 function App() {
   const [isOpen, setOpen] = useState(false);
   const myDivRef = useRef<HTMLDivElement>(null);
-  
-  const handleClick = ()=>{
-    if(myDivRef.current){
-      myDivRef.current.scrollIntoView({ behavior: 'smooth' });
+
+  const handleClick = () => {
+    if (myDivRef.current) {
+      myDivRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }
+
+    setOpen(false);
+  };
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
   return (
     <Wrapper>
       <Header>
         <div>
           <a href="/" className="logo">
-            newsc
+            nwsc
           </a>
         </div>
 
-        <div className="menu">
+        <div className="menuicon">
           <Hamburger
             toggled={isOpen}
             toggle={setOpen}
-            color="#ffffff"
+            color="#ff0000"
             easing="ease-in"
-            hideOutline={false}
+            hideOutline={true}
             size={28}
           />
         </div>
       </Header>
       {isOpen && (
         <Menu>
-          <a href="" className="menuitem">
+          <div className="menuitem" onClick={handleClick}>
             ჩვენს შესახებ
-          </a>
-          <a href="" className="menuitem">
+          </div>
+          <div className="menuitem" onClick={handleClick}>
             კონტაქტი
-          </a>
-          <a href="" className="menuitem">
-            დაჯავშნა
-          </a>
+          </div>
+          <div className="menuitem">დაჯავშნა</div>
+
+          <div className="zinikela" onClick={() => console.log("emzari")}>
+            zina
+          </div>
         </Menu>
       )}
 
       <video src={videoBG} autoPlay loop muted />
       <div className="bg-overley"> </div>
 
-      <motion.div className="o"
-      initial={{opacity:0.5}}
-      animate={{opacity: 1, transform: 'scale(1)'}}
-      transition={{type: 'spring'}}
-
+      <motion.div
+        className="o"
+        initial={{ opacity: 0.5 }}
+        animate={{ opacity: 1, transform: "scale(1)" }}
+        transition={{ type: "spring" }}
       >
-        <button className="order"
-        onClick={handleClick}
-        >დაჯავშნე</button>
+        <button className="order" onClick={handleClick}>
+          დაჯავშნე
+        </button>
       </motion.div>
 
-       
       <Content ref={myDivRef}>
-      <motion.div className="content"
-        initial={{opacity: 0}}
-        whileInView={{opacity:1, transform: 'scale(1)'}}
+        <motion.div
+          className="content"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1, transform: "scale(1)" }}
         >
           <div className="ordersection">
             <h1> rent a studio</h1>
             <p>develop yourself</p>
-           
           </div>
 
-          <p className="text">
-          We have a unique space for creative events and special occasions, we also have private studios for hire! Our incredible space in Prahran homes our music school The SoundLab and shares the same floor of our 700 sqm warehouse conversion, with a theatre company.
-          </p>
+          <motion.p className="text" variants={item}>
+            We have a unique space for creative events and special occasions, we
+            also have private studios for hire! Our incredible space in Prahran
+            homes our music school The SoundLab and shares the same floor of our
+            700 sqm warehouse conversion, with a theatre company.
+          </motion.p>
 
-          <p className="calltext">
+          <motion.p
+            className="calltext"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 100, x: -100, transform: "scale(1)" }}
+          >
             for rent call us:
-          </p>
-          <button className="call" onClick={()=>window.open("tel:+995568116969")}>call</button>
-
+          </motion.p>
+          <motion.button
+            className="call"
+            onClick={() => window.open("tel:+995568116969")}
+            initial={{ y: 100, opacity: 0.1 }}
+            whileInView={{ y: 0, opacity: 1 }}
+          >
+            call
+          </motion.button>
         </motion.div>
       </Content>
 
-   
-
-
-
-         
+      <div className="zina">
+        <motion.p
+          className="gallery"
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ type: "spring" }}
+        >
+          Gallery
+        </motion.p>
+        <motion.div
+          className="ziniko"
+          initial={{ x: -50, opacity: 0 }}
+          whileInView={{ opacity: 1, scale: 1, x: 1 }}
+          transition={{ type: "spring" }}
+        >
+          <motion.div
+            className="photos"
+            variants={container}
+            initial="hidden"
+            animate="visible"
+          >
+            {imgArray.map((pic) => (
+              <motion.div key={pic} className="item" variants={item}>
+                <img src={pic} alt="" />
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
+        <motion.p
+          className="p"
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+        >
+          ALL RIGHT RESERVED 
+          <p className='bottomtext'>
+          © 2023 for
+          <span className="logonottom"> NWSC</span>  studio
+          </p>
+        </motion.p>
+      </div>
     </Wrapper>
   );
 }
 
 export default App;
-
 
 const Wrapper = styled.div`
   width: 100%;
@@ -123,18 +195,16 @@ const Header = styled.div`
   z-index: 3;
 `;
 
-
-
 const Menu = styled.div`
-  background-color: #b09e9e34;
+  background-color: #cecece16;
   position: absolute;
   top: 50px;
   height: 92%;
   width: 50%;
   right: 0;
-  transform: matrix();
   display: flex;
   flex-direction: column;
+  z-index: 100;
 `;
 
 const Content = styled.div`
